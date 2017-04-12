b61b99b97663b86af165d761132be95c0efe6542
WebmetsHD <webmets.alt1@gmail.com>
2017-04-12 17:59:46 +0200

    Create Inventoryholder.java

---
+++ src/com/webmets/forceop/Inventoryholder.java
@@ -0,0 +1,14 @@
+package com.webmets.forceop;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class Inventoryholder implements InventoryHolder {
+
+	@Override
+	public Inventory getInventory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
